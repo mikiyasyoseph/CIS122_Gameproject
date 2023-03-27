81b2b7878ee92126353be8a707e043eeb9269170
mikiyasyoseph <122575428+mikiyasyoseph@users.noreply.github.com>
2023-03-26 20:27:25 -0500

    Create Base stats for enemies

---
+++ Assets/Enemy/EnemyStats.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    [SerializeField] protected int health;
+    [SerializeField] protected int maxHealth;
+    [SerializeField] protected bool isDead;
+    private void Start()
+    {
+        InitVariables();
+    }
+    public virtual void CheckHealth()
+    {
+        if(health <= 0)
+        {
+            health= 0;
+        }
+        if(health >= maxHealth) 
+        {
+            health= maxHealth;
+        }
+    }
+    public virtual void Die()
+    {
+        isDead = true;
+    }
+    public void SetHealthTo(int healtToSetTo)
+    {
+        health = healtToSetTo;
+        CheckHealth();
+    }
+    public void TakeDamage(int damage) 
+    {
+        int healthAfterDamage = health - damage;
+        SetHealthTo(healthAfterDamage);
+    }
+    public void Heal(int heal)
+    {
+        int healthAfterHeal = health + heal;
+        SetHealthTo(healthAfterHeal);
+    }
+    public virtual void InitVariables()
+    {
+        maxHealth = 100;
+        SetHealthTo(maxHealth);
+        isDead = false;
+    }
+}
